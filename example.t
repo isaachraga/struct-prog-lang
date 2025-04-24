b3d254bb03665df988dd9fe4bbd531580de0a345
isaachraga <isaachraga@yahoo.com>
2025-04-24 14:30:30 +0000

    repl update

---
+++ example.t
@@ -0,0 +1,9 @@
+print 1+2+3;
+print 4+-4;
+print 3 > 4;
+print 3 % 2;
+homework = 1;
+if (homework==1) {
+   ihraga;
+   print(_kentid_)
+}
--- topic-07-functions/runner.py
+++ topic-07-functions/runner.py
@@ -9,17 +9,22 @@ def run(text):
     evaluator.evaluate(ast)
 
 def repl():
-    print("Welcome to your sopl REPL!")
+    print("Welcome to your sopl REPL!\n")
+    print("Available commands: info, exit/quit\n")
 
     while True:
         try:
             line = input(">>>> ")
             if line.strip() in {"exit", "quit"}:
-                print("Goodbye!")
+                print("Closing sopl...")
                 break
+            if line.strip() in {"info"}:
+                print("Uses the synax of our in-class language\n")
+                print("Need to figure out multiline\n")
+                continue;
             run(line)
         except Exception as e:
-            print("❌ Error:", e)
+            print("Error:", e)
 
 
 if __name__ == "__main__":
