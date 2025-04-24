cfe76e2282c27b7c000a484046f500ed5b69c54b
isaachraga <isaachraga@yahoo.com>
2025-04-24 14:07:28 +0000

    added repl

--- topic-07-functions/runner.py
+++ topic-07-functions/runner.py
@@ -8,10 +8,26 @@ def run(text):
     ast = parser.parse(tokens)
     evaluator.evaluate(ast)
 
+def repl():
+    print("Welcome to your sopl REPL!")
+
+    while True:
+        try:
+            line = input(">>>> ")
+            if line.strip() in {"exit", "quit"}:
+                print("Goodbye!")
+                break
+            run(line)
+        except Exception as e:
+            print("❌ Error:", e)
+
+
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         with open(sys.argv[1],"r") as f:
             source = f.read()
         run(source)
+    else:
+        repl()
 
 
